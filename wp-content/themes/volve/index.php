c95511f25e3eaa841a170a064db31eddc93cc8e9
Yevhenii Kalinichenko <iamyevhenii@gmail.com>
2024-03-11 18:02:41 +0200

    prepared theme folder for creating templates

---
+++ wp-content/themes/volve/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html <?php language_attributes(); ?> >
+<head>
+    <meta charset="<?php bloginfo('charset');?> ">
+    <?php wp_head(); ?>
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body <?php body_class('example'); ?>>
+<?php wp_body_open(); ?>
+<p>Hello word</p>
+
+<?php wp_footer(); ?>
+</body>
+</html>
