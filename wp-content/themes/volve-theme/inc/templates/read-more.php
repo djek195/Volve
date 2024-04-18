edab2065470b290bb14cd7f51ebcee3c2a3c7263
Yevhenii Kalinichenko <djek195@gmail.com>
2024-04-18 14:05:45 +0300

    created read more block

---
+++ wp-content/themes/volve-theme/inc/templates/read-more.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @var array $fields , an array with the entered data in the block.
+ * @var array $attributes , an array with the attributes of the block like custom CSS class, alignment, etc.
+ * @var string $inner_blocks , a string with the content of all nested blocks.
+ */
+?>
+
+<div class="read-more__wrapper">
+    <h2 class="read-more__title">Read more</h2>
+    <div class="swiper read-more__slider">
+        <div class="swiper-wrapper">
+            <?php foreach ($fields['posts'] as $post): ?>
+                <div class="read-more__slide swiper-slide">
+                    <div class="read-more__slide--image">
+                        <?php echo get_the_post_thumbnail($post->ID, 'full') ?>
+                    </div>
+                    <div class="read-more__slide--text-content">
+                        <h3 class="read-more__slide--text-content-title"><?= $post->post_title ?></h3>
+                        <p class="read-more__slide--text-content-description"><?= $post->post_excerpt ?></p>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+        <div class="read-more__slide--buttons">
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+    </div>
+</div>
