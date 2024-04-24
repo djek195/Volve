85c3fe44a3867b06860c31c5366e8f408ee5acc2
Yevhenii Kalinichenko <djek195@gmail.com>
2024-04-24 12:53:28 +0300

    Updated PrivilegesTabs

--- wp-content/themes/volve-theme/inc/blocks/PrivilegesTabs.php
+++ wp-content/themes/volve-theme/inc/blocks/PrivilegesTabs.php
@@ -6,14 +6,19 @@ use Carbon_Fields\Field;
 Block::make(__('Privileges Tabs', 'practice'))
     ->add_fields(
         [
-            Field::make( 'complex', 'crb_slider', __( 'Slider' ) )
-                ->add_fields( array(
-                    Field::make( 'text', 'title', __( 'Slide Title' ) ),
-                ) )
+            Field::make('text', 'title', __('Title')),
+            Field::make('complex', 'crb_tab', __('Privileges Tabs'))
+                ->add_fields(array(
+                    Field::make('text', 'tab_title', __('Tab Title')),
+                    Field::make('complex', 'tab_content', __('Comparison'))
+                        ->add_fields(array(
+                            Field::make('text', 'comparison_subtitle', __('Comparison Title')),
+                            Field::make('checkbox', 'comparison_volve', __('Comparison Volve')),
+                            Field::make('checkbox', 'comparison_others', __('Comparison Others')),
+                        )),
+                ))->set_layout('tabbed-horizontal')
         ]
     )
-
-
     ->set_render_callback(function ($fields, $attributes, $inner_blocks) {
         custom_block_render('privileges-tabs', $fields, $attributes, $inner_blocks);
     });
