26817f1eb5152c5e37a5790bb9aca777f3f34cec
Yevhenii Kalinichenko <djek195@gmail.com>
2024-04-26 17:55:28 +0300

    added support for title tags

--- wp-content/themes/volve-theme/functions.php
+++ wp-content/themes/volve-theme/functions.php
@@ -104,6 +104,8 @@ add_theme_support('menus');
 
 add_theme_support('post-thumbnails');
 
+add_theme_support('title-tag');
+
 add_action('after_setup_theme', 'my_cool_menu_function');
 
 add_action('wp_enqueue_scripts', 'volve_theme_scripts');
